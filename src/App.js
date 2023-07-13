811c6183657f588761eb4253655e20642cb229cd
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-13 16:47:45 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import EditableBlock from './components/EditableBlock';
+import EditablePage from './components/EditablePage';
+import SelectMenu from './components/SelectMenu';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EditableBlock/>
+      <EditablePage/>
+      <SelectMenu/>
     </div>
   );
 }
